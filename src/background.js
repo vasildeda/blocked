eca38489c8a2532843b0c33e657135125b7aa330
Filip Novák <filipnovak@filipnovak.cz>
2022-11-24 13:33:45 +0100

    Remove tabId

--- src/background.js
+++ src/background.js
@@ -4,29 +4,29 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
         if (changeInfo.status == 'loading' && tab.active) {
-            handleLoading(tabId)
+            handleLoading()
         }
     })
 })
 
-function doRedirect(tabId) {
+function doRedirect() {
     isRunning(running => {
         if (running) {
-            chrome.tabs.update(tabId, {
+            chrome.tabs.update(null, {
                 url: chrome.runtime.getURL("./blocked.html")
             });        
         }
     })
 }
 
-function handleLoading(tabId) {
+function handleLoading() {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         let url = tabs[0].url;
         if (url) {
             getPatterns(patterns => {
                 for (let pattern of patterns) {
                     if (url.includes(pattern)) {
-                        doRedirect(tabId)
+                        doRedirect()
                     }
                 }
             })
