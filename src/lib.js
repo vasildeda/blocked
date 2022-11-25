b0f4fc1a4f159944c2c0ccd54bf913579c3b20be
Filip Novák <filipnovak@filipnovak.cz>
2022-11-25 12:49:44 +0100

    Set storage type once

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,7 @@
+const storage = chrome.storage.sync
 
 function getPatterns(callback) {
-    chrome.storage.sync.get(['patterns'], result => {
+    storage.get(['patterns'], result => {
         let patterns = result['patterns'] || []
         callback(patterns)
     })
@@ -8,7 +9,7 @@ function getPatterns(callback) {
 
 function addPattern(pattern) {
     getPatterns(patterns => {
-        chrome.storage.sync.set({
+        storage.set({
             patterns: patterns.concat(pattern)
         })
     })
@@ -16,27 +17,27 @@ function addPattern(pattern) {
 
 function removePattern(pattern) {
     getPatterns(patterns => {
-        chrome.storage.sync.set({
+        storage.set({
             patterns: patterns.filter(e => e != pattern)
         })
     })
 }
 
 function isRunning(callback) {
-    chrome.storage.sync.get(['running'], result => {
+    storage.get(['running'], result => {
         let running = result['running']
         callback(running)
     })
 }
 
 function setRunning(running) {
-    chrome.storage.sync.set({
+    storage.set({
         running: running
     })
 }
 
 function getCount(pattern, callback) {
-    chrome.storage.sync.get(['calls'], result => {
+    storage.get(['calls'], result => {
         const calls = result['calls'] || {}
         const patternCalls = calls[pattern] || 0
         callback(patternCalls)
@@ -44,19 +45,19 @@ function getCount(pattern, callback) {
 }
 
 function incrementCount(pattern) {
-    chrome.storage.sync.get(['calls'], result => {
+    storage.get(['calls'], result => {
         const calls = result['calls'] || {}
         calls[pattern] = calls[pattern] || 0
         calls[pattern] += 1
 
-        chrome.storage.sync.set({
+        storage.set({
             calls: calls
         })
     })
 }
 
 function getTotal(callback) {
-    chrome.storage.sync.get(['calls'], result => {
+    storage.get(['calls'], result => {
         const calls = result['calls'] || {}
         const total = Object
             .values(calls)
